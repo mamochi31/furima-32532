7ff1a826cfb4a0c01b361b3d0488ea79ea86fd2c
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-08 16:45:06 +0900

    ActiveHash使用に伴うモデルファイル編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipment_date
+  belongs_to :category
+
 
   with_options presence: true do
-    validates :name, :price, :text, :status_id, :shipping_fee_id, :prefecture_id,
-              :shipment_date_id, :category_id
+    validates :name, :price, :text
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :status_id, :shipping_fee_id, :prefecture_id, :shipment_date_id, :category_id
   end
 
 end
