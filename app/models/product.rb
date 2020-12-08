09dfabb3076b3ac755b9e0330b8a59088f54e114
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-08 16:00:34 +0900

    バリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  with_options presence: true do
+    validates :name, :price, :text, :status_id, :shipping_fee_id, :prefecture_id,
+              :shipment_date_id, :category_id
+  end
+
 end
