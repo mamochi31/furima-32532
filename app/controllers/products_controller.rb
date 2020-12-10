e0b228aa7403249d662a9385aa241e19c4645f58
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-10 15:55:01 +0900

    出品者以外は編集ページに遷移できないよう設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, only: :edit
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -42,4 +43,10 @@ class ProductsController < ApplicationController
       :name, :price, :text, :image, :status_id, :shipping_fee_id, :prefecture_id, :shipment_date_id, :category_id
     ).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    redirect_to action: :index if current_user.id != @product.user_id
+  end
+
 end
