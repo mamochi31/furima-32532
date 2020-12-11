c745270def8e07db900ab72a2a02dde4a3ad5393
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-11 18:24:08 +0900

    test

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,11 +1,28 @@
 class BuyersController < ApplicationController
-  def index
-    @product = Product.find(params[:product_id])
-  end
+  before_action :find_product
 
-  def new
+  def index
+    @buyer = Buyer.new
   end
 
   def create
+    @buyer = Buyer.new(buyer_params)
+    if buyer.valid?
+      @buyer.save
+      return redirect_to product_buyers_path(@product.id)
+    else
+      render :index
+    end
   end
+
+  private
+
+  def buyer_params
+    params.require(:buyer).permit(:price)
+  end
+
+  def find_product
+    @product = Product.find(params[:product_id])
+  end
+
 end
