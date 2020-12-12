b77999c1e22f9d47c67d263e638598e3233f7b20
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-12 14:37:52 +0900

    Formオブジェクトの修正

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,6 +1,6 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :price
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -11,8 +11,13 @@ class BuyerAddress
   end
 
   def save
+    user = User.find(params[:user_id])
+    product = Product.find(params[:product_id])
     Address.create(
       postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
       address: address, building: building, phone_number: phone_number
     )
+    Buyer.create(user_id: user.id, product_id: product.id)
+  end
+
 end
