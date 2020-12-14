a22bd8edef2658ee5c9c014b3deb018a080f26b7
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-14 18:51:21 +0900

    購入機能のテストコード記述

---
+++ spec/factories/buyer_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buyer_address do
+    token { "tok_abcdefghijk00000000000000000" }
+    postal_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Gimei.city.kanji }
+    address { Gimei.town.kanji }
+    phone_number { Faker::Number.number(digits: 11) }
+  end
+end
