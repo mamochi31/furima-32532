94cacfef03fd467d74729e4556819f0f81602a16
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-14 14:13:42 +0900

    住所保存の設定

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,23 +1,22 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :price
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :buyer_id, :user_id, :product_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select"}
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
   end
 
   def save
-    user = User.find(params[:user_id])
-    product = Product.find(params[:product_id])
+    buyer = Buyer.create(user_id: user_id, product_id: product_id)
+    
     Address.create(
       postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
-      address: address, building: building, phone_number: phone_number
+      address: address, building: building, phone_number: phone_number, buyer_id: buyer.id
     )
-    Buyer.create(user_id: user.id, product_id: product.id)
   end
 
 end
