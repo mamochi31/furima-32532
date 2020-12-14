1fb8463f0ae81ac286f3b727a016d9ed5a9b3d5d
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-14 14:12:55 +0900

    コントローラーの修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,11 +1,27 @@
 class BuyersController < ApplicationController
+  before_action :find_product
+  
   def index
-    @product = Product.find(params[:product_id])
-  end
-
-  def new
+    @buyer_address = BuyerAddress.new
   end
 
   def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.permit(:postal_code, :prefecture_id,:city, :address, :buildind, :phone_number, :product_id).merge(user_id: current_user.id)
+  end
+
+  def find_product
+    @product = Product.find(params[:product_id])
   end
 end
