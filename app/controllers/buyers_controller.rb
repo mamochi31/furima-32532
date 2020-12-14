58342ca2e341e7e09c846842676cffc2844e37a3
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-14 15:40:47 +0900

    クレジット決済機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,6 +8,7 @@ class BuyersController < ApplicationController
   def create
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
+      pay_product
       @buyer_address.save
       redirect_to root_path
     else
@@ -18,10 +19,21 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.permit(:postal_code, :prefecture_id,:city, :address, :buildind, :phone_number, :product_id).merge(user_id: current_user.id)
+    params.permit(
+      :postal_code, :prefecture_id,:city, :address, :buildind, :phone_number, :product_id
+    ).merge(user_id: current_user.id, token: params[:token])
   end
 
   def find_product
     @product = Product.find(params[:product_id])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,6 +1,6 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :buyer_id, :user_id, :product_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :buyer_id, :user_id, :product_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
@@ -8,6 +8,8 @@ class BuyerAddress
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
+
+    validates :token
   end
 
   def save
